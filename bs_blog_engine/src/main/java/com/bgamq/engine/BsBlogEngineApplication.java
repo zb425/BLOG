efd3a2a5afbc0ddef77866d07b3cc4325a24c953
sen588 <lisen19980328>
2020-05-12 16:03:19 +0800

    更新engine启动类

--- bs_blog_engine/src/main/java/com/bgamq/engine/BsBlogEngineApplication.java
+++ bs_blog_engine/src/main/java/com/bgamq/engine/BsBlogEngineApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EngineApplication {
+public class BsBlogEngineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EngineApplication.class, args);
+        SpringApplication.run(BsBlogEngineApplication.class, args);
     }
 
 }
